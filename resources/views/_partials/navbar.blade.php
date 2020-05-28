2234d86e99b8701e62cd96078b84afd72bfbc6c3
salmankp <salmankp88@gmail.com>
2020-05-28 20:46:20 +0500

    navbar set

---
+++ resources/views/_partials/navbar.blade.php
@@ -0,0 +1,74 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <img src="{{ asset('assets/img/logo-dark.png') }}" alt="" height="40px" width="150px"/>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active" style="padding-left: 20px">
+{{--                <a href="{{url('/home')}}" class="active">--}}
+                <a class="nav-link" href="{{url('/home')}}">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Add Product</a>
+            </li>
+
+            @can('users-page')
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Users
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="">All Users</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="">Add User</a>
+                </div>
+            </li>
+            @endcan
+        </ul>
+
+        <ul class="nav navbar-nav ml-auto">
+
+            <li class="nav-item dropdown dropdown-slide" style="margin-right:30px;">
+                <a class="nav-link nav-pill user-avatar" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    <img src="{{ asset('assets/img/user.png') }}" alt="John Doe">
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-menu-accout">
+                    <div class="dropdown-header pb-3">
+                        <div class="media d-user">
+                            <img class="align-self-center mr-3" src="{{ asset('assets/img/user.png') }}" alt="John Doe" height="50px" width="50px">
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-0">{{ auth()->user()->name }}</h5>
+                                <span>{{ auth()->user()->email }}</span>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <a class="dropdown-item" href="{{ url('change-password') }}"><i class=" ti-lock"></i> Change Password</a>
+
+                    <div class="dropdown-divider"></div>
+
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        <i class=" ti-unlock"></i>        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+
+
+                </div>
+            </li>
+
+        </ul>
+{{--        <form class="form-inline my-2 my-lg-0">--}}
+{{--            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">--}}
+{{--            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>--}}
+{{--        </form>--}}
+    </div>
+</nav>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,72 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+<!--===========header start===========-->
+    @include('_partials.navbar')
+
+    <!--===========app body start===========-->
+    <div class="app-body">
+
+        <!--main contents start-->
+        <main class="main-content main_area_width">
+
+            <div class="container-fluid">
+
+                <!--page title start-->
+{{--                <div class="page-title pl-0">--}}
+{{--                    <h4 class="mb-0"> Home--}}
+{{--                    </h4>--}}
+{{--                    <ol class="breadcrumb mb-0 pl-0 pt-1 pb-0">--}}
+{{--                        <li class="breadcrumb-item active">/home</li>--}}
+{{--                    </ol>--}}
+{{--                </div>--}}
+                <!--page title end-->
+
+                <!--state widget start-->
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-3">
+                                            <span class="bg-primary rounded-circle text-center wb-icon-box">
+                                                <i class="icon-people text-light f24"></i>
+                                            </span>
+                                    </div>
+                                    <div class="col-9">
+                                        <h6 class="mt-1 mb-0">Total Products</h6>
+                                        <p class="f12 mb-0"></p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
+                    </div>
 
-                    You are logged in!
+                    <div class="col-lg-4 col-md-6">
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-3">
+                                            <span class="bg-primary rounded-circle text-center wb-icon-box">
+                                                <i class="icon-people text-light f24"></i>
+                                            </span>
+                                    </div>
+                                    <div class="col-9">
+                                        <h6 class="mt-1 mb-0">Total Users</h6>
+                                        <p class="f12 mb-0"></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <!--state widget end-->
+
             </div>
-        </div>
+        </main>
+        <!--main contents end-->
+
     </div>
-</div>
+    <!--===========app body end===========-->
+    @include('_partials.footer')
 @endsection
