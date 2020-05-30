f8930f65a1b706792dc811b791f15184275e105e
salmankp <salmankp88@gmail.com>
2020-05-30 15:49:09 +0500

    aws s3 configured

--- app/Image.php
+++ app/Image.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Image extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = [
+        'user_id', 'product_id', 'file_name', 'image_url',
+    ];
 }
--- database/migrations/2020_05_28_095000_add_columns_to_users_table.php
+++ database/migrations/2020_05_28_095000_add_columns_to_users_table.php
@@ -17,7 +17,8 @@ class AddColumnsToUsersTable extends Migration
             $table->string('username')->nullable()->after('email');
             $table->string('role')->nullable()->after('username');
             $table->boolean('status')->nullable()->default(0)->after('role')->comment('0=inactive , 1=active');
-            $table->string('extra_field_1')->nullable()->after('status');
+            $table->integer('created_by')->nullable()->after('status');
+            $table->string('extra_field_1')->nullable()->after('created_by');
             $table->string('extra_field_2')->nullable()->after('extra_field_1');
             $table->string('extra_field_3')->nullable()->after('extra_field_2');
             $table->softDeletes();
